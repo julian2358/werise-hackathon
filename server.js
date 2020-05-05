67b712aaa0924fdadf9b65db4b33fc928f8fe726
julian2355 <37557548+julian2355@users.noreply.github.com>
2020-05-05 10:01:13 -0400

    files

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+//port 
+const PORT = process.env.PORT || 3000;
+
+
+//test
+app.get('/', (req, res) => {
+        res.send('hello world')
+})
+
+app.listen(PORT, () => {
+    console.log(`server is running on ${PORT}`)
+}); 
