993cb88289ffd65ac0834f9bb0e425d541ca1206
julian2355 <37557548+julian2355@users.noreply.github.com>
2020-05-06 16:46:41 -0400

    set up express server

--- server.js
+++ server.js
@@ -1,18 +1,49 @@
+const request = require('request');
 const express = require('express');
 const app = express();
+const path = require('path');
 
 //port 
 const PORT = process.env.PORT || 3000;
+app.set('view engine', 'ejs');
 
+
+
+// add css and images
+app.use(express.static('public'))
+app.use(express.static(__dirname + '/public'));
 
 //test
-app.get('/', (req, res) => {
-        res.send('hello world')
+// routing
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+app.get('/index.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.get('/about.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/about.html'));
+});
+
+app.get('/greennewdeal.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/greennewdeal.html'));
+});
+app.get('/donate.html', function(req, res) {
+
+    res.sendFile(path.join(__dirname + '/donate.html'));
+});
+
+app.get('/contact.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/contact.html'));
+});
+
+app.get('/carbonfootprint.html', function(req, res) {
+    res.sendFile(path.join(__dirname + '/carbonfootprint.html'));
+});
+
+
+const port = process.env.PORT || 3042;
+app.listen(port, () => {
+    console.log(`this server is running on ${port}`)
 })
-
-app.listen(PORT, () => {
-    console.log(`server is running on ${PORT}`)
-}); 
-
-
-
