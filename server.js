5bd82b7d3fe1472d19c0fecf109432765eb67107
julian2358 <julianreeves2358@gmail.com>
2020-05-07 09:10:22 -0400

    server

--- server.js
+++ server.js
@@ -47,7 +47,8 @@ app.get('/political.html', function(req, res) {
 });
 
 
-const port = process.env.PORT || 3021;
+const port = process.env.PORT || 3223;
 app.listen(port, () => {
+
     console.log(`this server is running on ${port}`)
 })
